7a120de65207ac6c23f077658982665f780110f6
Zahran812 <167998013+Zahran812@users.noreply.github.com>
2024-12-23 22:54:17 +0700

    Pesan commit Anda

--- controllers/KonserController.php
+++ controllers/KonserController.php
@@ -1,34 +1,18 @@
 <?php
+require_once '../config/koneksi.php';
 
-class KonserController {
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "db_konser"; // Ganti dengan nama database Anda
-
-    // Koneksi ke database
-    private function getConnection() {
-        $conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-        if ($conn->connect_error) {
-            die("Koneksi gagal: " . $conn->connect_error);
-        }
-        return $conn;
-    }
-
-    // Mendapatkan semua data peserta konser
+class KonserController extends Koneksi {
     public function getPeserta() {
         $conn = $this->getConnection();
-        $sql = "SELECT * FROM tblPesertaKonser"; // Sesuaikan dengan nama tabel Anda
+        $sql = "SELECT * FROM tblPesertaKonser";
         $result = $conn->query($sql);
         $data = [];
         while ($row = $result->fetch_assoc()) {
             $data[] = $row;
         }
-        $conn->close();
         return $data;
     }
 
-    // Mendapatkan data peserta berdasarkan ID
     public function getPesertaById($id) {
         $conn = $this->getConnection();
         $sql = "SELECT * FROM tblPesertaKonser WHERE id = ?";
@@ -38,11 +22,9 @@ class KonserController {
         $result = $stmt->get_result();
         $data = $result->fetch_assoc();
         $stmt->close();
-        $conn->close();
         return $data;
     }
 
-    // Menambahkan peserta baru
     public function insertPeserta($nama, $umur, $kategori) {
         $conn = $this->getConnection();
         $sql = "INSERT INTO tblPesertaKonser (nama, umur, kategori) VALUES (?, ?, ?)";
@@ -50,11 +32,9 @@ class KonserController {
         $stmt->bind_param("sis", $nama, $umur, $kategori);
         $result = $stmt->execute();
         $stmt->close();
-        $conn->close();
         return $result;
     }
 
-    // Memperbarui data peserta
     public function updatePeserta($id, $nama, $umur, $kategori) {
         $conn = $this->getConnection();
         $sql = "UPDATE tblPesertaKonser SET nama = ?, umur = ?, kategori = ? WHERE id = ?";
@@ -62,11 +42,9 @@ class KonserController {
         $stmt->bind_param("sisi", $nama, $umur, $kategori, $id);
         $result = $stmt->execute();
         $stmt->close();
-        $conn->close();
         return $result;
     }
 
-    // Menghapus data peserta
     public function deletePeserta($id) {
         $conn = $this->getConnection();
         $sql = "DELETE FROM tblPesertaKonser WHERE id = ?";
@@ -74,7 +52,6 @@ class KonserController {
         $stmt->bind_param("i", $id);
         $result = $stmt->execute();
         $stmt->close();
-        $conn->close();
         return $result;
     }
 }
